f9611a8fd4c8de9966f41c700ce57f1d3c786b20
Boominathan <eboominathan@gmail.com>
2024-03-20 21:54:15 +0530

    counter added

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import counterReducer from "../feature/counter/counterSlice";
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    
+  },
+});
